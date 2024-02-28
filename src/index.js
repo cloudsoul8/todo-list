317b82f0f3ec09bb6ea92d8e6efc415b3b8152db
Cloud Soul <stoutshako42ref.zglj9@slmail.me>
2024-02-28 19:27:43 +0800

    added 'add todo', 'delete todo', 'add group' functionality

--- src/index.js
+++ src/index.js
@@ -23,11 +23,38 @@ function addTodo(group, title, description, dueDate, priority) {
 }
 
 function deleteTodo(group, delTitle, delDescription) {
-    const delIndex = group.groupList.findIndex((title, description) => {
-        return title === delTitle, description === delDescription
-    })
-    group.groupList.splice(delIndex, 1)
+    const delIndex = group.groupList.findIndex(
+        todo => todo.title === delTitle && todo.description == delDescription 
+    )
+    if (delIndex != -1)
+        group.groupList.splice(delIndex, 1)
 }
+
+function changeTodo(todo) {
+    todo.isComplete = !todo.isComplete
+}
+
+
+// json.js
+// initialization, change to json
+let todoListMain = []
+function loadTodoJSON() {
+    if (localStorage.getItem("todoListMain") === null) {
+        let defaultGroup = new TodoGroup("default", "this is the default group")
+        addTodo(defaultGroup, "Welcome!", "Hello there!", "some date1", "high")
+        addTodo(defaultGroup, "Welcome!", "Hello there, too!", "some date1", "high")
+        todoListMain = [defaultGroup]
+        console.log("No localStorage file found. Placed defaults.")
+    } else {
+        todoListMain = JSON.parse(localStorage.todoListMain)
+        console.log("Loaded localStorage.")
+    }
+}
+
+function saveTodoJSON() {
+    localStorage.setItem("todoListMain", JSON.stringify(todoListMain))
+}
+
 
 // dom.js
 function loadTodoGroup(group) {
@@ -42,7 +69,7 @@ function loadTodoGroup(group) {
         // change todo items
         document.getElementById("todo-list").innerText = ""
         group.groupList.forEach((todo) => {
-            todoListDiv.appendChild(loadTodos(todo))
+            todoListDiv.appendChild(loadTodos(group, todo))
         })
     })
     return groupItem
@@ -55,7 +82,7 @@ function loadTodoHeader(group) {
     todoHeader.append(Object.assign(document.createElement('h2'),{classList:"", innerText:group.groupDescription}))
 }
 
-function loadTodos(todo) {
+function loadTodos(group, todo) {
     const todoItem = document.createElement("div")
     todoItem.classList.add("todo-item")
 
@@ -66,27 +93,54 @@ function loadTodos(todo) {
     todoItem.append(Object.assign(document.createElement('p'),{classList:"", innerText:todo.isComplete}))
 
     // add button to delete (probably similar to library)
-
+    const deleteBtn = document.createElement("button")
+    deleteBtn.setAttribute("class", 'delete-btn')
+    deleteBtn.addEventListener("click", event => {
+        deleteTodo(group, todo.title, todo.description)
+        saveTodoJSON()
+    })
+    todoItem.appendChild(deleteBtn)
     return todoItem
 }
 
+function loadTodoGroupDropdown() {
+
+}
+
+
+
 // index.js
-// initialization, change to json
-let group1 = new TodoGroup("project1", "this is a cool project")
-let group2 = new TodoGroup("project2", "this is a cool project, too")
-let todoListMain= [group1, group2]
-addTodo(group1, "yeah", "desc", "some date1", "high")
-addTodo(group1, "yeah", "desc", "some date2", "high")
-addTodo(group1, "yeah", "desc", "some date3", "high")
-addTodo(group2, "yeah", "desc", "some date4", "high")
-addTodo(group2, "yeah", "desc", "some date5", "high")
+loadTodoJSON()
 
 
 // dom manip
 const todoGroupDiv = document.getElementById("todo-group")
+
+
 const todoListDiv = document.getElementById("todo-list")
 todoListMain.forEach((group) => {
     todoGroupDiv.appendChild(loadTodoGroup(group))
 })
 
-
+const addTodoBtn = document.getElementById("add-todo-btn")
+addTodoBtn.addEventListener("click", event => {
+    
+})
+const addTodoConfirm = document.getElementById("add-todo-confirm")
+addTodoConfirm.addEventListener("click", event => {
+    const title = document.getElementById("add-todo-title").value;
+    const description = document.getElementById("add-todo-desc").value;
+    const dueDate = document.getElementById("add-todo-date").value;
+    const priority = document.getElementById("add-todo-priority").value;
+    const group = todoListMain[todoListMain.findIndex(group => group.groupName === document.getElementById("add-todo-group").value)]
+    addTodo(group, title, description, dueDate, priority);
+    saveTodoJSON();
+})
+const addGroupConfirm = document.getElementById("add-group-confirm")
+addGroupConfirm.addEventListener("click", event => {
+    const title = document.getElementById("add-group-title").value;
+    const description = document.getElementById("add-group-desc").value;
+    todoListMain.push(new TodoGroup(title, description))
+    saveTodoJSON();
+    console.log(1)
+})
